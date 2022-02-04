28facd9d32ca16616dd790155c9d7563c40b9873
chenkaixu@lab <chenkaixusan@gmail.com>
2022-02-04 08:26:10 +0000

    update

---
+++ models/U_Net.py
@@ -0,0 +1,84 @@
+'''
+U-Net structure.
+'''
+#%%
+import torch
+import torch.nn as nn
+
+def double_conv(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, 3, padding=1),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(out_channels, out_channels, 3, padding=1),
+        nn.ReLU(inplace=True)
+    )   
+
+
+class UNet(nn.Module):
+
+    def __init__(self, n_class):
+        super().__init__()
+                
+        self.dconv_down1 = double_conv(3, 64)
+        self.dconv_down2 = double_conv(64, 128)
+        self.dconv_down3 = double_conv(128, 256)
+        self.dconv_down4 = double_conv(256, 512)        
+
+        self.maxpool = nn.MaxPool2d(2)
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)        
+        
+        self.dconv_up3 = double_conv(256 + 512, 256)
+        self.dconv_up2 = double_conv(128 + 256, 128)
+        self.dconv_up1 = double_conv(128 + 64, 64)
+        
+        self.conv_last = nn.Conv2d(64, n_class, 1)
+        
+        
+    def forward(self, x):
+        conv1 = self.dconv_down1(x)
+        x = self.maxpool(conv1)
+
+        conv2 = self.dconv_down2(x)
+        x = self.maxpool(conv2)
+        
+        conv3 = self.dconv_down3(x)
+        x = self.maxpool(conv3)   
+        
+        x = self.dconv_down4(x)
+        
+        x = self.upsample(x)        
+        x = torch.cat([x, conv3], dim=1)
+        
+        x = self.dconv_up3(x)
+        x = self.upsample(x)        
+        x = torch.cat([x, conv2], dim=1)       
+
+        x = self.dconv_up2(x)
+        x = self.upsample(x)        
+        x = torch.cat([x, conv1], dim=1)   
+        
+        x = self.dconv_up1(x)
+        
+        out = self.conv_last(x)
+        
+        return out
+
+# %%
+from torchinfo import summary
+
+if __name__ == "__main__":
+    
+    unet = UNet(6)
+
+    batch_size = 16
+    summary(unet, input_size=(batch_size, 3, 224, 224))
+
+# %%
+# import pprint
+
+# # get model summary as string 
+# model_stats = summary(unet, (1, 3, 224, 224), verbose=0)
+# summary_str = str(model_stats)
+# with open('model_structure.log', 'w') as tf:
+#     pprint.pprint(summary_str, stream=tf)
+# %%
